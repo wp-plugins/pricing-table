610947a7f1113bc4b6fab3224ea1916dfdc19845
codename065 <codename065@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-30 23:14:07 +0000

    git-svn-id: https://plugins.svn.wordpress.org/pricing-table/trunk@469706 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- pricing-table.php
+++ pricing-table.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Pricing table
 Plugin URI: http://shaon.info/pricing-table-builder-plugin-for-wordpress/
-Description: Display important message/notice from site admin to visitor. Use simple short-code <strong>[ahm-pricing-table id=999]</strong> ( <strong>999</strong> = use any table id here) inside page or post content to embed pricing table
+Description: Generate Pricing Table Easily. Use simple short-code <strong>[ahm-pricing-table id=999]</strong> ( <strong>999</strong> = use any table id here) inside page or post content to embed pricing table
 Author: Shaon
 Version: 1.0.1
 Author URI: http://shaon.info/
